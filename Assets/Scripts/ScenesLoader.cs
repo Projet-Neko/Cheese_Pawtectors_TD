f239058698da8dccf9bac4ec914147a007ec0ca3
Crimson41 <111068020+Crimson41@users.noreply.github.com>
2024-02-28 01:46:53 +0100

    switch loadScene method to naughtyAttributes

---
+++ Assets/Scripts/ScenesLoader.cs
@@ -0,0 +1,19 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesLoader : MonoBehaviour
+{
+    [Scene]
+    public string Scenes;
+
+    //private void Start()
+    //{
+    //    SceneManager.LoadScene("LoginPopup", LoadSceneMode.Additive);
+    //}
+
+    //public void OnSettingsButtonClick()
+    //{
+    //    SceneManager.LoadScene("Settings", LoadSceneMode.Additive);
+    //}
+}

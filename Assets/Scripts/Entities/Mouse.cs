2d16ab308b9ca259fb20982c8c31f8f0c111e843
Justine-dt <72021396+Justine-dt@users.noreply.github.com>
2024-02-29 10:30:20 +0100

    add final hetahBAr

--- Assets/Scripts/Entities/Mouse.cs
+++ Assets/Scripts/Entities/Mouse.cs
@@ -19,20 +19,23 @@ public class Mouse : Entity
         _level = GameManager.Instance.MouseLevel;
 
         _baseHealth = _currentHealth = _data.Health + (_level * 1) - 1;
-        _healthBar.SetMaxHealth(_maxHealth);
         _damage = _data.SatiationRate;
         _speed = _data.Speed;
-
+        _currentHealth = 5;
         //_renderer.sprite = _data.Sprite;
+        _healthBar.SetMaxHealth();
 
         gameObject.name = _data.Name;
     }
 
     void Update()
     {
+        Entity mouse = this;
+
         if (Input.GetKeyDown(KeyCode.I))
         {
-            TakeDamage(2);
+            mouse.TakeDamage(_healthBar);
+            SetHealth();
         }
     }
     private int IsAlbino()
@@ -46,19 +49,14 @@ public class Mouse : Entity
         return 0;
     }
 
-    public void SetMaxHealth(int health)
+    public void SetMaxHealth()
     {
-        _slider.maxValue = health;
-        _slider.value = health;
+        _slider.maxValue = _maxHealth;
+        _slider.value = _maxHealth;
     }
 
-    public void SetHealth(int health)
+    public void SetHealth()
     {
-        _slider.value = health;
-    }
-
-    void TakeDamage(int damage)
-    {
-        _currentHealth -= damage;
+        _slider.value = _currentHealth;
     }
 }

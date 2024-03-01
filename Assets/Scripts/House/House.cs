e9135df948da00beff3b96bbe0b123fd8f196695
Titouan <titouan.lefloch@orange.fr>
2024-03-01 22:02:19 +0100

    Add start Script House and Room

---
+++ Assets/Scripts/House/House.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    private List<Room> roomsAvailable = new List<Room>();
+    private Room[,] rooms = new Room[30,30];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

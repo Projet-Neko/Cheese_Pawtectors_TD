4bf3d789e6155daf7d64174637ab8e097f5a8296
Titouan <titouan.lefloch@orange.fr>
2024-03-20 23:37:41 +0100

    start path

--- Assets/Scripts/House/Room/Junction.cs
+++ Assets/Scripts/House/Room/Junction.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class Junction : MonoBehaviour
 {
-    private bool _connected = false;
+    private bool _connected = false;    // True if the Junction is connected to another Junction
+    private bool _valided = false;      // True if the Junction is belong to the valided path
 
     public bool Connected { get => _connected; }
+    public bool Valided { get => _valided; }
 
     private void OnCollisionEnter(Collision collision)
     {
         _connected = true;
+
+        Junction junction = collision.gameObject.GetComponent<Junction>();
+        if (junction && junction.Valided)
+        {
+            _valided = true;
+            // Notify room that the path is valided
+        }
     }
 
     private void OnCollisionExit(Collision collision)

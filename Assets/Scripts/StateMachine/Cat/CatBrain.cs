5e64404303d6cb1fa786d35b30e9da612abe3d5d
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-07 17:35:44 +0200

    fix cat sleep on satiety full

--- Assets/Scripts/StateMachine/Cat/CatBrain.cs
+++ Assets/Scripts/StateMachine/Cat/CatBrain.cs
@@ -6,4 +6,10 @@ public class CatBrain : Brain
         _attackRange = transform.localScale.x * 8;
         _followRange = (transform.localScale.x * 4) * 8;
     }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (_currentState is not State_Sleep && !_entity.IsAlive()) ChangeState(Sleep);
+    }
 }

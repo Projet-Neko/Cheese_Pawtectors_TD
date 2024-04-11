ce12a33a28ec2e989fdc0e71b9a5ea5170fdd17d
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-11 12:50:15 +0200

    add raycast3D, still trying to fix raycast 2D

--- Assets/Scripts/Controllers/DragAndDrop.cs
+++ Assets/Scripts/Controllers/DragAndDrop.cs
@@ -11,18 +11,32 @@ public class DragAndDrop : MonoBehaviour
 
     private bool _isBeingDragged = false;
     private Vector3 _initialPosition;
+    private Vector3 _offset;
 
     private void OnMouseDrag()
     {
         if (!_isBeingDragged) return;
 
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+        /*Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);*/
+
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + _offset;
+    }
+
+    private Vector3 RoundPosition(Vector3 startPosition)
+    {
+        Vector3 position = startPosition;
+        position.x = Mathf.Round(position.x);
+        position.y = 0;
+        position.z = Mathf.Round(position.z);
+
+        return position;
     }
 
     private void OnMouseDown()
     {
         if (GameManager.Instance.IsPopupSceneLoaded) return;
+        _offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition); // Calcul de l'offset
         Grab(true);
     }
 
@@ -30,7 +44,27 @@ public class DragAndDrop : MonoBehaviour
     {
         if (!_isBeingDragged) return;
 
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Raycast en 3D pour les rooms
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit3D;
+
+        // Raycast en 2D pour l'entrepôt
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 60f; // Ajuster la coordonnée z à la position z du canvas
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        RaycastHit2D hit2D = Physics2D.Raycast(worldPosition, Vector2.zero);
+
+        if (Physics.Raycast(ray, out hit3D))
+        {
+            Debug.Log("Objet touché : " + hit3D.collider.gameObject.name);
+        }
+        else if (hit2D.collider != null)
+        {
+            Debug.Log("Objet touché : " + hit2D.collider.gameObject.name);
+        }
+        else transform.position = _initialPosition;
+
+        /*Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = -1f;
         RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
@@ -39,8 +73,7 @@ public class DragAndDrop : MonoBehaviour
             //Debug.Log($"raycast on {hit.collider.gameObject.name}");
             if (_cat != null) component.HandleDragAndDrop(_cat, _initialPosition);
             else if (_room != null) component.HandleDragAndDrop(_room, _initialPosition);
-        }
-        else transform.position = _initialPosition;
+        }*/
 
         Grab(false);
     }

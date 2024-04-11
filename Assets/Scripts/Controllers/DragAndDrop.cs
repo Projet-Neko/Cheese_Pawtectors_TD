7f4512e592354f1271208a9b00a33fbd809b8f59
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-11 16:30:12 +0200

    fix drag and drop with 3D and iso camera

--- Assets/Scripts/Controllers/DragAndDrop.cs
+++ Assets/Scripts/Controllers/DragAndDrop.cs
@@ -13,67 +13,45 @@ public class DragAndDrop : MonoBehaviour
     private Vector3 _initialPosition;
     private Vector3 _offset;
 
+    private void Awake()
+    {
+        _canvas = GetComponentInParent<Canvas>();
+    }
+
     private void OnMouseDrag()
     {
         if (!_isBeingDragged) return;
-
-        /*Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);*/
-
         transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + _offset;
-    }
-
-    private Vector3 RoundPosition(Vector3 startPosition)
-    {
-        Vector3 position = startPosition;
-        position.x = Mathf.Round(position.x);
-        position.y = 0;
-        position.z = Mathf.Round(position.z);
-
-        return position;
     }
 
     private void OnMouseDown()
     {
         if (GameManager.Instance.IsPopupSceneLoaded) return;
-        _offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition); // Calcul de l'offset
         Grab(true);
+        _offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void OnMouseUp()
     {
         if (!_isBeingDragged) return;
 
-        // Raycast en 3D pour les rooms
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit3D;
+        RaycastHit2D hit2D = Physics2D.GetRayIntersection(ray);
 
-        // Raycast en 2D pour l'entrepôt
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = 60f; // Ajuster la coordonnée z à la position z du canvas
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        RaycastHit2D hit2D = Physics2D.Raycast(worldPosition, Vector2.zero);
-
-        if (Physics.Raycast(ray, out hit3D))
+        if (Physics.Raycast(ray, out RaycastHit hit3D)) // Raycast en 3D pour les pièces
         {
             Debug.Log("Objet touché : " + hit3D.collider.gameObject.name);
         }
-        else if (hit2D.collider != null)
+        else if (hit2D.collider != null) // Raycast en 2D pour l'entrepôt
         {
             Debug.Log("Objet touché : " + hit2D.collider.gameObject.name);
+            if (hit2D.collider != null && hit2D.collider.gameObject.TryGetComponent(out DragAndDropHandler component))
+            {
+                if (_cat != null) component.HandleDragAndDrop(_cat, _initialPosition);
+                else if (_room != null) component.HandleDragAndDrop(_room, _initialPosition);
+            }
         }
         else transform.position = _initialPosition;
-
-        /*Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = -1f;
-        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
-
-        if (hit.collider != null && hit.collider.gameObject.TryGetComponent(out DragAndDropHandler component))
-        {
-            //Debug.Log($"raycast on {hit.collider.gameObject.name}");
-            if (_cat != null) component.HandleDragAndDrop(_cat, _initialPosition);
-            else if (_room != null) component.HandleDragAndDrop(_room, _initialPosition);
-        }*/
 
         Grab(false);
     }

e89689a7cc69076c6dd85c9c8c7ed75eaa694cf6
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-13 13:21:35 +0200

    starting drop cat on rooms

--- Assets/Scripts/House/DropCat.cs
+++ Assets/Scripts/House/DropCat.cs
@@ -8,10 +8,12 @@ public class DropCat : DragAndDropHandler
     public override void HandleDragAndDrop(Cat cat, Vector3 initialPosition)
     {
         if (transform.childCount == 0) _currentCat = null;
-        _previousSlotIndex = int.Parse(cat.transform.parent.name.Split('_')[1]);
+        //_previousSlotIndex = int.Parse(cat.transform.parent.name.Split('_')[1]);
         // TODO -> vérifier si le chat faisait partie d'une room plutôt d'un slot
 
         _currentCat = cat;
-        cat.transform.position = Camera.main.ScreenToWorldPoint(cat.transform.position);
+        _currentCat.transform.SetParent(transform);
+        _currentCat.transform.position = transform.position;
+        //cat.transform.position = Camera.main.ScreenToWorldPoint(cat.transform.position);
     }
 }

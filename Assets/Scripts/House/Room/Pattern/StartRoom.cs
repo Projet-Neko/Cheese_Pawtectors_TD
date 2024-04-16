a87e1d8b06ddf731449bb10987cf267c5f082510
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-16 16:54:25 +0200

    spawn mouses on start room

--- Assets/Scripts/House/Room/Pattern/StartRoom.cs
+++ Assets/Scripts/House/Room/Pattern/StartRoom.cs
@@ -1,9 +1,12 @@
-using UnityEngine.Rendering;
+using System;
 
 public class StartRoom : Room
 {
+    public static event Action<Room> OnInit;
+
     void Awake()
     {
         _security = RoomSecurity.Protected;
+        OnInit?.Invoke(this);
     }
 }

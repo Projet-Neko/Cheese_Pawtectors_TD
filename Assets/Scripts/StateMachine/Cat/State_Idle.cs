663dcd82c04cfa0e9e3684cceab64b65641e1853
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-16 17:24:30 +0200

    prevent cat from leaving its room

--- Assets/Scripts/StateMachine/Cat/State_Idle.cs
+++ Assets/Scripts/StateMachine/Cat/State_Idle.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class State_Idle : State
 {
     public override void OnUpdate()
@@ -5,6 +7,14 @@ public class State_Idle : State
         base.OnUpdate();
 
         if ((_brain.Entity as Cat).IsInStorageMode) return;
+
+        while (_brain.Room == null)
+        {
+            if (_brain.Entity.transform.parent.gameObject.TryGetComponent(out BoxCollider collider))
+            {
+                _brain.Room = collider;
+            }
+        }
 
         // TODO -> Add idle movements
 

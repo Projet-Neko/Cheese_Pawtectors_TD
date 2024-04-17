81cba088cc83b8b9c0d690f97b8037fa603d9182
Lyna <91870976+Sasha697@users.noreply.github.com>
2024-04-17 18:13:00 +0200

    change set popup

--- Assets/Scripts/UI/Catalog/CatalogPopup.cs
+++ Assets/Scripts/UI/Catalog/CatalogPopup.cs
@@ -5,27 +5,17 @@ using UnityEngine.UI;
 public class CatalogPopup : MonoBehaviour
 {
     public static event Action<bool> OnToggle;
-    public ScrollRect scrollRect;
-    private string scrollPositionKey = "ScrollPosition";
 
     private void OnEnable()
     {
         OnToggle?.Invoke(true);
         AdoptButton.OnAdoptButtonClick += AdoptButton_OnAdoptButtonClick;
-        // Load saved ScrollRect position on popup activation
-        Vector2 savedPosition = new Vector2(PlayerPrefs.GetFloat(scrollPositionKey + "X", 0f),
-                                            PlayerPrefs.GetFloat(scrollPositionKey + "Y", 0f));
-        scrollRect.normalizedPosition = savedPosition;
     }
 
     private void OnDisable()
     {
         OnToggle?.Invoke(false);
         AdoptButton.OnAdoptButtonClick -= AdoptButton_OnAdoptButtonClick;
-        // Save the current position of the ScrollRect when the popup is deactivated
-        PlayerPrefs.SetFloat(scrollPositionKey + "X", scrollRect.normalizedPosition.x);
-        PlayerPrefs.SetFloat(scrollPositionKey + "Y", scrollRect.normalizedPosition.y);
-        PlayerPrefs.Save(); // Save changes in PlayerPrefs
     }
 
     private void AdoptButton_OnAdoptButtonClick(int obj)
--- Assets/Scripts/UI/SetPopup.cs
+++ Assets/Scripts/UI/SetPopup.cs
@@ -1,11 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetPopup : MonoBehaviour
 {
+    public ScrollRect scrollRect;
+    private string scrollPositionKey = "ScrollPosition";
+
     private void Awake()
     {
         Canvas canvas = GetComponent<Canvas>();
         canvas.worldCamera = Camera.main;
         //canvas.sortingOrder = 10;
+
+        scrollRect = GetComponentInChildren<ScrollRect>(); // Trouver la ScrollRect dans les enfants du canvas
+        if (scrollRect == null)
+        {
+            Debug.LogError("ScrollRect not found in children.");
+            return;
+        }
+
+        // Load saved ScrollRect position on popup activation
+        Vector2 savedPosition = new Vector2(PlayerPrefs.GetFloat(scrollPositionKey + "X", 0f),
+                                            PlayerPrefs.GetFloat(scrollPositionKey + "Y", 0f));
+        scrollRect.normalizedPosition = savedPosition;
+    }
+
+    private void OnDisable()
+    {
+        if (scrollRect == null)
+        {
+            // Save the current position of the ScrollRect when the popup is deactivated
+            PlayerPrefs.SetFloat(scrollPositionKey + "X", scrollRect.normalizedPosition.x);
+            PlayerPrefs.SetFloat(scrollPositionKey + "Y", scrollRect.normalizedPosition.y);
+            PlayerPrefs.Save(); // Save changes in PlayerPrefs
+        }
     }
 }

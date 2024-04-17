d85206288acc08129762ef22b11bb44f0df477a8
Lyna <91870976+Sasha697@users.noreply.github.com>
2024-04-17 17:54:25 +0200

    save scrollPosition for the catalog need testing needed

--- Assets/Scripts/UI/Catalog/CatalogPopup.cs
+++ Assets/Scripts/UI/Catalog/CatalogPopup.cs
@@ -1,14 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatalogPopup : MonoBehaviour
 {
     public static event Action<bool> OnToggle;
+    public ScrollRect scrollRect;
+    private string scrollPositionKey = "ScrollPosition";
 
     private void OnEnable()
     {
         OnToggle?.Invoke(true);
         AdoptButton.OnAdoptButtonClick += AdoptButton_OnAdoptButtonClick;
+        // Load saved ScrollRect position on popup activation
+        Vector2 savedPosition = new Vector2(PlayerPrefs.GetFloat(scrollPositionKey + "X", 0f),
+                                            PlayerPrefs.GetFloat(scrollPositionKey + "Y", 0f));
+        scrollRect.normalizedPosition = savedPosition;
     }
 
     private void OnDisable()

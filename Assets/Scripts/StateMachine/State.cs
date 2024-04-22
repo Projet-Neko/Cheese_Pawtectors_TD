1b981d1d13e8212ee8f35220f9a61c91a16b84de
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-22 17:38:32 +0200

    load main screen async

--- Assets/Scripts/StateMachine/State.cs
+++ Assets/Scripts/StateMachine/State.cs
@@ -68,7 +68,6 @@ public abstract class State
 
     protected void FollowTarget()
     {
-       
         _brain.transform.position = Vector3.MoveTowards(_brain.transform.position, _brain.Target.transform.position, _brain.Entity.Speed * Time.deltaTime);
         if (_brain.Entity is not Cat) return;
         _brain.SpriteDirection(_brain.Target.transform.position);

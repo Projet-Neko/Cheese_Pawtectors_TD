64d3f6b58d7b2524c769d455b569174b5bc73816
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-23 15:28:41 +0200

    fix error popup

--- Assets/Scripts/UI/InitError.cs
+++ Assets/Scripts/UI/InitError.cs
@@ -5,8 +5,9 @@ public class InitError : MonoBehaviour
 {
     [SerializeField] private TMP_Text _errorMessage;
 
-    private void Awake()
+    private void Start()
     {
+        Debug.LogWarning(GameManager.Instance.ErrorMessage);
         _errorMessage.text = GameManager.Instance.ErrorMessage;
     }
 }

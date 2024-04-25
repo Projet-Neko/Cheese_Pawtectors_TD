47fbd3beb6fb65710be06f1c8a74e1b18f21ed78
Emilie Gonin <emilie.yuu@gmail.com>
2024-04-25 15:17:07 +0200

    fix clan

--- Assets/Scripts/SO/ClanSO.cs
+++ Assets/Scripts/SO/ClanSO.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "NewClan", menuName = "Projet Neko/Clan")]
 public class ClanSO : ScriptableObject
 {
     public string Description;
-    public Image Gem;
+    public Sprite Gem;
 }
